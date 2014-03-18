58036f53e35a8adeb25686768a3acbc6342b05a8
Todd Horst <toddmhorst@yahoo.com>
2014-03-18 15:10:47 -0400

    fixed name

---
+++ dist/simple.0.0.1.js
@@ -0,0 +1,16 @@
+/*! Simple Js - v0.0.1 - 2014-03-18
+* https://github.com/thorst/Grunt.Qunit.istanbul
+* Copyright (c) 2014 Todd Horst; Licensed MIT */
+(function ($) {
+
+    $.simpleAdd = function (a,b) {
+		return a+b;
+	};
+	$.simpleSubtract = function (a,b) {
+		return a-b;
+	};
+	$.simpleMultiply = function (a,b) {
+		return a*b;
+	};
+
+})(jQuery);
--- dist/simple.js.min.js
+++
@@ -1,3 +0,0 @@
-/*! Simple Js - v0.0.1 - 2014-03-18
-* https://github.com/thorst/Grunt.Qunit.istanbul
-* Copyright (c) 2014 Todd Horst; Licensed MIT */
