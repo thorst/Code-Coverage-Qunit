8740a0f28337f2a94bd989008cda2d28824f53f5
Todd Horst <toddmhorst@yahoo.com>
2014-03-18 14:34:34 -0400

    1

---
+++ src/simple.js
@@ -0,0 +1,19 @@
+/*
+Since this is just an example
+Define some bogus functions
+*/
+
+
+
+function add(a,b) {
+	return a+b;
+}
+
+function subtract(a,b) {
+	return a-b;
+}
+
+function multiply(a,b) {
+	return a*b;
+}
+
---
+++ test/test.js
@@ -0,0 +1,35 @@
+(function($) {
+/*
+		======== A Handy Little QUnit Reference ========
+		http://docs.jquery.com/QUnit
+
+		Test methods:
+			expect(numAssertions)
+			stop(increment)
+			start(decrement)
+		Test assertions:
+			ok(value, [message])
+			equal(actual, expected, [message])
+			notEqual(actual, expected, [message])
+			deepEqual(actual, expected, [message])
+			notDeepEqual(actual, expected, [message])
+			strictEqual(actual, expected, [message])
+			notStrictEqual(actual, expected, [message])
+			raises(block, [expected], [message])
+*/
+    //Name | expects | test
+	
+	
+	module("Test only add");
+	
+
+	test("Test Add", 2, function () {
+
+		equal(typeof add(1,2), "number", "Add type should be a number");
+		equal(add(1,2), 3, "Add Works");
+		
+	});
+
+	
+
+}(jQuery));
